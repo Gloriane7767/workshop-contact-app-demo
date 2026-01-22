009f1ee6584d604f596b8796425a13d567f42f37
Glory Ngum Foncha <gloriane7767@gmail.com>
2026-01-22 08:56:41 +0100

    create duplicate Contact  exception class with methods in exception package

---
+++ src/main/java/com/gloriane/exception/DuplicateContactException.java
@@ -0,0 +1,8 @@
+package com.gloriane.exception;
+
+public class DuplicateContactException extends Exception {
+    public DuplicateContactException(String message) {
+        super(message);
+    }
+}
+
